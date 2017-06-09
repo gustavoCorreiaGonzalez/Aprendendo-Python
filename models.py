ea9a2db5fa89ba2d402e0c3c9242e161e4b40245
gustavo <gustavogonzalez@alunos.utfpr.edu.br>
2017-06-09 16:00:30 -0300

    Encapsulamento em python

--- models.py
+++ models.py
@@ -6,6 +6,15 @@ class Perfil(object):
 		self.nome = nome
 		self.telefone = telefone
 		self.empresa = empresa
+		# deixa o atribulo "privado" -> p pytho gera um nome aleatório para o atributo em todos os
+		# lugares aonde ele é usado para o desenvolvedor não conseguir acessar o atributo
+		self.__curtidas = 0
 	
 	def imprimir(self):
 		print 'Nome: %s, Telefone: %s, Empresa: %s' % (self.nome, self.telefone, self.empresa)
+
+	def curtir(self):
+		self.__curtidas += 1
+
+	def obter_curtidas(self):
+		return self.__curtidas
