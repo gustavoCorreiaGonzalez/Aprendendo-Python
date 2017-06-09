3578800fa0837f69282974b36a4cbcbd6441cd1c
gustavo <gustavogonzalez@alunos.utfpr.edu.br>
2017-06-09 15:42:34 -0300

    Aprendendo Classes

---
+++ models.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+
+class Perfil(object):
+	
+	def __init__(self, nome, telefone, empresa):
+		self.nome = nome
+		self.telefone = telefone
+		self.empresa = empresa
+	
+	def imprimir(self):
+		print 'Nome: %s, Telefone: %s, Empresa: %s' % (self.nome, self.telefone, self.empresa)
